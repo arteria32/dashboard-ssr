adce6385464da36ced823212e2997851f5787c5b
arteria <denis.rjabko@mail.ru>
2025-01-05 14:58:16 +0300

    add button to create new dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,25 @@
-import Plug from '@/_shared/components/plug/plug';
+import { Button, Flex, Link } from '@gravity-ui/uikit';
 import '@gravity-ui/uikit/styles/fonts.css';
 import '@gravity-ui/uikit/styles/styles.css';
 import { FC } from 'react';
-
-export const dynamic = 'force-dynamic';
+import styles from './dashboard.module.scss';
 
 const InitDashboardPage: FC = async () => {
-  return <Plug message={'Дефолтная страница  '} />;
+  return (
+    <div className={styles.page}>
+      <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        gap={3}
+        className={styles.buttonsList}
+      >
+        <Link href="./create-dashboard">
+          <Button size="xl">Create new dashboard</Button>
+        </Link>
+      </Flex>
+    </div>
+  );
 };
 
 export default InitDashboardPage;
