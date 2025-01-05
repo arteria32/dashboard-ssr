70b92f7aa118a6e044fec736de2522c690dbcfee
arteria <denis.rjabko@mail.ru>
2025-01-05 16:03:40 +0300

    add create page

---
+++ src/app/dashboard/create/page.tsx
@@ -0,0 +1,17 @@
+import { Flex, Text } from '@gravity-ui/uikit';
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uiOkit/styles/styles.css';
+import { FC } from 'react';
+import styles from './create.module.scss';
+
+const CreateDashboardPage: FC = async () => {
+  return (
+    <div className={styles.page}>
+      <Flex direction="column" gap={3}>
+        <Text>Create new dashboard</Text>
+      </Flex>
+    </div>
+  );
+};
+
+export default CreateDashboardPage;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
-import { Button, Flex, Link } from '@gravity-ui/uikit';
+import { Button, Flex } from '@gravity-ui/uikit';
 import '@gravity-ui/uikit/styles/fonts.css';
 import '@gravity-ui/uikit/styles/styles.css';
 import { FC } from 'react';
 import styles from './dashboard.module.scss';
+import Link from 'next/link';
 
 const InitDashboardPage: FC = async () => {
   return (
@@ -14,7 +15,7 @@ const InitDashboardPage: FC = async () => {
         gap={3}
         className={styles.buttonsList}
       >
-        <Link href="./create-dashboard">
+        <Link href="./dashboard/create">
           <Button size="xl">Create new dashboard</Button>
         </Link>
       </Flex>
